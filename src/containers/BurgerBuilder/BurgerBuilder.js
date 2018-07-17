1f5a0d7b64046cd6346f49ae9a3b4fd51d83d08a
Garrison, Lloyd <lgarrison@def-logix.com>
2018-07-17 12:21:23 -0500

    Added Buttons to OrderSummary/Checkout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -78,6 +78,10 @@ class BurgerBuilder extends Component {
     purchaseCancelHandler = () => {
         this.setState({purchasing: false});
     }
+
+    purchaseContinueHandler = () => {
+        alert('You continue!');
+    }
     
     render () {
         const disabledInfo = {
@@ -89,7 +93,10 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients}/>
+                    <OrderSummary 
+                        ingredients={this.state.ingredients}
+                        purchaseCancelled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}/>
                 </Modal>/>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
