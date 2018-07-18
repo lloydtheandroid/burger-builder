de2fd8a4aa802ba1570bef6185e16698951e80fb
Garrison, Lloyd <lgarrison@def-logix.com>
2018-07-18 09:04:00 -0500

    Applied Drawer Toggle to the Layout

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -3,7 +3,9 @@ import React from 'react';
 import classes from './DrawerToggle.css'
 
 const drawerToggle = (props) => (
-    <div>Menu</div>
+    <div 
+        className={classes.DrawerToggle}
+        onClick={props.clicked}>Menu</div>
 );
 
 export default drawerToggle;
