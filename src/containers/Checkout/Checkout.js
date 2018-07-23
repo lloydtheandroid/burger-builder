19b159725e2216f8723df1c903a567c6506b56d3
Garrison, Lloyd <lgarrison@def-logix.com>
2018-07-23 14:13:03 -0500

    added Contact Data form

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
-
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state = {
@@ -11,6 +12,16 @@ class Checkout extends Component {
             cheese: 1,
             bacon: 1
         }
+    }
+    
+    componentDidMount () {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+        for (let param of query.entries()){
+            // ['salad', '1']
+            ingredients[param[0]] = +param[1];
+        }
+        this.setState({ingredients: ingredients});
     }
 
     checkoutCancelledHandler = () => {
@@ -28,6 +39,9 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinueHandler}/>
+                <Route 
+                    path={this.props.match.url + '/contact-data'}
+                    component={ContactData}/>
             </div>
         )
     }
