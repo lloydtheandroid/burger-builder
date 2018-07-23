ef57ed74d40c6d8de4502dd75a65b3f7883735b5
Garrison, Lloyd <lgarrison@def-logix.com>
2018-07-23 15:15:58 -0500

    added Orders page

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -21,7 +21,7 @@ const burger = (props) => {
     return (
         <div className={classes.Burger}>
           <BurgerIngredient type="bread-top"/>
-          {transformedIngredients}
+            {transformedIngredients}
           <BurgerIngredient type="bread-bottom"/>
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -86,33 +86,11 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // alert('You continue!');
-        // this.setState({loading: true});
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Lloyd Garrison',
-        //         address: {
-        //             street: 'Magic Ave',
-        //             zipCode: '78210',
-        //             country: 'USA'
-        //         },
-        //         email: 'test@test.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({loading: false, purchasing: false});
-        //     })
-        //     .catch(error => {
-        //         this.setState({loading: false, purchasing: false})
-        //     });
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&'); 
         this.props.history.push({
             pathname: '/checkout',
