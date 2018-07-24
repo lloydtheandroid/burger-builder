8a13974cef657c3bd74f1614bf15b8ad81197aa4
Garrison, Lloyd <lgarrison@def-logix.com>
2018-07-24 12:11:06 -0500

    finished up form validation, fixed dropdown bug

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -19,7 +19,8 @@ class ContactData extends Component {
                     validation: {
                         required:true
                     },
-                    valid: false
+                    valid: false,
+                    touched: false
             },
             street: {
                 elementType: 'input',
@@ -31,7 +32,9 @@ class ContactData extends Component {
                 validation: {
                     required:true
                 },
-                valid: false
+                valid: false,
+                touched: false
+
             },
             zipCode: {
                 elementType: 'input',
@@ -45,7 +48,9 @@ class ContactData extends Component {
                     minLength: 5,
                     maxLength: 5
                 },
-                valid: false
+                valid: false,
+                touched: false
+
             },
             country: {
                 elementType: 'input',
@@ -57,7 +62,9 @@ class ContactData extends Component {
                 validation: {
                     required:true
                 },
-                valid: false
+                valid: false,
+                touched: false
+
             },
             email: {
                 elementType: 'input',
@@ -69,7 +76,9 @@ class ContactData extends Component {
                 validation: {
                     required:true
                 },
-                valid: false
+                valid: false,
+                touched: false
+
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -79,9 +88,12 @@ class ContactData extends Component {
                         {value: 'cheapest', displayValue: 'Cheapest'},
                     ]
                 },
-                value: ''
+                value: 'fastest',
+                validation: {},
+                valid: true
             }
         },
+        formIsValid: false,
         loading: false
     }
 
@@ -109,6 +121,9 @@ class ContactData extends Component {
 
     checkValidity(value, rules) {
         let isValid = true;
+        if (!rules) {
+            return true;
+        }
 
         if (rules.required) {
             isValid = value.trim() !== '' && isValid;
@@ -132,9 +147,15 @@ class ContactData extends Component {
         };
         updatedFormElement.value = event.target.value;
         updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+        updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
-        console.log(updatedFormElement);
-        this.setState({orderForm: updatedOrderForm});
+        
+        let formIsValid = true;
+        for (let inputIdentifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+
+        this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
     }
 
     render () {
@@ -155,9 +176,10 @@ class ContactData extends Component {
                         value={formElement.config.value}
                         invalid={!formElement.config.valid}
                         shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)} />
                 ))}
-                <Button btnType="Success" >ORDER</Button>
+                <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
             </form>
         );
         if ( this.state.loading ) {
